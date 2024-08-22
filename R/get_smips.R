71022adffd2dd76a15e0ff575636b7ac9dab03b5
Adam H. Sparks <adam.sparks@curtin.edu.au>
2024-08-22 14:21:45 +0800

    fix link to SMIPS

--- R/get_smips.R
+++ R/get_smips.R
@@ -2,6 +2,7 @@
 #' 
 #'  Soil Moisture Integration and Prediction System (\acronym{SMIPS})
 
+#' @references <https://portal.tern.org.au/metadata/TERN/d1995ee8-53f0-4a7d-91c2-ad5e4a23e5e0https://geonetwork.tern.org.au/geonetwork/srv/eng/catalog.search#/metadata/d1995ee8-53f0-4a7d-91c2-ad5e4a23e5e0>
 get_smips <- function(start_date, end_date) {
 
 }
