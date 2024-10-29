a888cc66208410bf7111da1bca1dcfcf1f4b4660
Adam H. Sparks <adam.sparks@curtin.edu.au>
2024-10-29 17:56:42 +0800

    Fix `SMindex` and rename `dates` to `day`, fix spelling

--- R/get_smips.R
+++ R/get_smips.R
@@ -4,33 +4,37 @@
 #'
 #' @param collection A character vector of the data collection to be queried,
 #'  on of:
-#'  * SMIndex
+#'  * SMindex
 #'  * bucket1
 #'  * bucket2
 #'  * deepD
 #'  * runoff
 #'  * totalbucket
 #'  Defaults to \dQuote{totalbucket}.
-#' @param dates A date to query, _e.g._, `date = 2017-12-31`, both `Character` or
-#'  `Date` classes are accepted.
+#' @param day A single day's date to query, _e.g._, `day = "2017-12-31"`, both
+#'  `Character` and `Date` classes are accepted.
 #'
 #' @examples
-#' r <- get_smips(dates = "2024-01-01")
+#' r <- get_smips(day = "2024-01-01")
 #'
-#' # terra::plot() is re-exported for convience
+#' # terra::plot() is re-exported for convenience
 #' plot(r)
 #'
 #' @return A [terra::rast] object
 #' @references <https://portal.tern.org.au/metadata/TERN/d1995ee8-53f0-4a7d-91c2-ad5e4a23e5e0https://geonetwork.tern.org.au/geonetwork/srv/eng/catalog.search#/metadata/d1995ee8-53f0-4a7d-91c2-ad5e4a23e5e0>
 #' @export
 get_smips <- function(collection = "totalbucket",
-                      dates,
+                      day,
                       api_key = get_key()) {
 
-  dates <- lubridate::ymd(dates)
-  url_dates <- gsub("-", "", dates)
+  day <- lubridate::ymd(day)
+  url_date <- gsub("-", "", day)
+  url_year <- lubridate::year(url_date)
+
+  .check_years(.collection = collection, .url_date = url_date)
+
   approved_collections <- c("totalbucket",
-                            "SMIndex",
+                            "SMindex",
                             "bucket1",
                             "bucket2",
                             "deepD",
@@ -39,17 +43,17 @@ get_smips <- function(collection = "totalbucket",
 
   collection_url <- data.table::fcase(
     collection == "totalbucket",
-    paste0("smips_totalbucket_mm_", url_dates, ".tif"),
-    collection == "SMIndex",
-    paste0("smips_smi_perc_", url_dates, ".tif"),
+    paste0("smips_totalbucket_mm_", url_date, ".tif"),
+    collection == "SMindex",
+    paste0("smips_smi_perc_", url_date, ".tif"),
     collection == "bucket1",
-    paste0("smips_bucket1_mm_", url_dates, ".tif"),
+    paste0("smips_bucket1_mm_", url_date, ".tif"),
     collection == "bucket2",
-    paste0("smips_bucket2_mm_", url_dates, ".tif"),
+    paste0("smips_bucket2_mm_", url_date, ".tif"),
     collection == "deepD",
-    paste0("smips_deepD_mm_", url_dates, ".tif"),
+    paste0("smips_deepD_mm_", url_date, ".tif"),
     collection == "runoff",
-    paste0("smips_runoff_mm_", url_dates, ".tif")
+    paste0("smips_runoff_mm_", url_date, ".tif")
   )
 
   r <- terra::rast(paste0("/vsicurl/https://",
@@ -57,7 +61,7 @@ get_smips <- function(collection = "totalbucket",
                     "@data.tern.org.au/model-derived/smips/v1_0/",
                    collection,
                    "/",
-                   lubridate::year(dates),
+                   url_year,
                    "/",
                    collection_url))
   return(r)
