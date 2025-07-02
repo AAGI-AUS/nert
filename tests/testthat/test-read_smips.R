5560e1888921ff5327aa6130b4466165fe3d0e8c
Adam H. Sparks <adam.sparks@curtin.edu.au>
2025-07-02 15:13:56 +0800

    skip tests on CI that require API key

--- tests/testthat/test-read_smips.R
+++ tests/testthat/test-read_smips.R
@@ -1,10 +1,12 @@
 test_that("get_smips returns the total bucket data for 2020-01-01", {
+  skip_on_ci()
   smips <- read_smips(day = "2020-01-01")
   expect_named(smips, "smips_totalbucket_mm_20200101")
 })
 
 
 test_that("get_smips returns the Soil Moisture index data for 2020-01-01", {
+  skip_on_ci()
   smips <- read_smips(day = "2020-01-01", collection = "SMindex")
   expect_named(smips, "smips_smi_perc_20200101")
 })
