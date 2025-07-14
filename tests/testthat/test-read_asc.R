2643e2be24fdb813c88584417d004bcf09fba2a2
Adam H. Sparks <adam.sparks@curtin.edu.au>
2025-07-14 16:58:32 +0800

    ensure tests are skipped on CRAN/R-universe

--- tests/testthat/test-read_asc.R
+++ tests/testthat/test-read_asc.R
@@ -1,4 +1,5 @@
 test_that("get_asc returns the soil classification data", {
+  skip_on_cran()
   skip_on_ci()
   asc <- read_asc()
   expect_named(asc, "Class")
@@ -6,6 +7,7 @@ test_that("get_asc returns the soil classification data", {
 
 
 test_that("get_smips returns the confusion index for soil class", {
+  skip_on_cran()
   skip_on_ci()
   asc <- read_asc(confusion_index = TRUE)
   expect_named(asc, "ASC_CI_C_P_AU_TRN_N.cog")
--- tests/testthat/test-read_smips.R
+++ tests/testthat/test-read_smips.R
@@ -1,4 +1,5 @@
 test_that("get_smips returns the total bucket data for 2020-01-01", {
+  skip_on_cran()
   skip_on_ci()
   smips <- read_smips(day = "2020-01-01")
   expect_named(smips, "smips_totalbucket_mm_20200101")
@@ -6,6 +7,7 @@ test_that("get_smips returns the total bucket data for 2020-01-01", {
 
 
 test_that("get_smips returns the Soil Moisture index data for 2020-01-01", {
+  skip_on_cran()
   skip_on_ci()
   smips <- read_smips(day = "2020-01-01", collection = "SMindex")
   expect_named(smips, "smips_smi_perc_20200101")
@@ -13,6 +15,7 @@ test_that("get_smips returns the Soil Moisture index data for 2020-01-01", {
 
 
 test_that("get_smips errors with an appropriate message if no day provided", {
+  skip_on_cran()
   skip_on_ci()
   expect_error(
     read_smips(collection = "SMindex"),
